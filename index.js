c04b052d8b590d25b35626d2d7ea1ddbbc876def
BlueSkyFish <blueskyfish@kirchnerei.de>
2017-09-30 22:01:28 +0200

    Add some comments and rework the reading of changes

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const fs = require('fs');
 const {Readable, Writable} = require('stream');
 const path = require('path');
 
+/**
+ * @name LineData
+ * @property {string} name the basename of the path
+ * @property {number} line the line number
+ * @property {string} text the line text.
+ */
+
 
 class ConsoleStreamWriter extends Writable {
 
@@ -25,10 +32,15 @@ class ConsoleStreamWriter extends Writable {
 	}
 }
 
+/**
+ * Endless stream reader reads the file on every change at the pathname.
+ * 
+ * The class pipes to the next pipe element a object structure from type {@link LineData}.
+ */
 class EndlessStreamReader extends Readable {
 
 	constructor(pathname, stats, readFromPosition = -1) {
-		super({objectMode: true})
+		super({objectMode: true});
 
 		this.pathname = pathname;
 		this.stats = stats;
@@ -50,67 +62,91 @@ class EndlessStreamReader extends Readable {
 	_read() {
 	}
 
+	/**
+	 * Try to process the file change.
+	 * @param {fs.Stats} stats the file stats
+	 */
 	onChanged(stats) {
-		const _that = this;
+		if (stats && stats.size != this.stats.size) {
 
-		_that._getFileDescriptor()
-			.then((fd) => {
-				if (!fd) {
-					return _that.push(null);
-				}
-				const position = (_that.position <= stats.size) ? _that.position : 0
-				const size = stats.size - position;
-				const buffer = Buffer.alloc(size, 0, 'utf8');
-				fs.read(fd, buffer, 0, size, _that.position, (err, readOfBytes) => {
-					if (err) {
-						return;
-					}
-					_that.buffer += buffer.toString('utf8', 0, readOfBytes);
-					const parts = _that.buffer.split(_that.lineSeparator);
-					_that.buffer = parts.pop();
-					_that.position = stats.size;
-
-					for (let i = 0; i < parts.length; i++) {
-						const data = {
-							name: path.basename(_that.pathname),
-							line: ++_that.lineNumber,
-							text: parts[i]
-						}
-						_that.push(data);
-					}
+			// get the file descriptor
+			this._getFileDescriptor()
+				.then((fd) => this._readFromFileDescriptor(stats, fd), (reason) => {
+					console.warn('Error on get file description: ', reason);
+					this.push(null);
 				});
-			}, (reason) => {
-				console.warn(reason);
-				_that.push(null);
-			});
+		}
 	}
 
+	/**
+	 * Try to read the file change and push to the next pipe element
+	 * @param {fs.Stats} stats
+	 * @param {number} fd the file descriptor
+	 * @private
+	 */
+	_readFromFileDescriptor(stats, fd) {
+		if (!fd) {
+			return this.push(null);
+		}
+		const position = (this.position <= stats.size) ? this.position : 0
+		const size = stats.size - position;
+		const buffer = Buffer.alloc(size, 0, 'utf8');
+
+		this.stats = stats;
+		
+		fs.read(fd, buffer, 0, size, this.position, (err, readOfBytes) => {
+			if (err) {
+				return;
+			}
+			this.buffer += buffer.toString('utf8', 0, readOfBytes);
+			const parts = this.buffer.split(this.lineSeparator);
+			this.buffer = parts.pop();
+			this.position = stats.size;
+
+			for (let i = 0; i < parts.length; i++) {
+				/** @type {LineData} */
+				const data = {
+					name: path.basename(this.pathname),
+					line: ++this.lineNumber,
+					text: parts[i]
+				}
+				this.push(data);
+			}
+		});
+	}
+
+	/**
+	 * Try to get the File Descriptor Id.
+	 * 
+	 * @return {Promise<number>} the promise resolve the file descriptor id.
+	 * @private
+	 */
 	_getFileDescriptor() {
-		const _that = this;
 		return new Promise((resolve, reject) => {
 			if (this.fd === false) {
 				// open the file
-				fs.open(_that.pathname, 'r', (err, fd) => {
+				// console.log('Open File: %s', this.pathname);
+				fs.open(this.pathname, 'r', (err, fd) => {
 					if (err) {
 						this.fd = null;
 						return reject({
-							pathname: _that.pathname,
+							pathname: this.pathname,
 							message: 'file not found'
 						});
 					}
-					_that.fd = fd;
-					resolve(_that.fd);
+					this.fd = fd;
+					resolve(this.fd);
 				});
 				return;
 			}
 			if (this.fd === null) {
 				// file not exist
 				return reject({
-					pathname: _that.pathname,
+					pathname: this.pathname,
 					message: 'file not found'
 				});
 			}
-			resolve(_that.fd);
+			resolve(this.fd);
 		});
 	}
 }
