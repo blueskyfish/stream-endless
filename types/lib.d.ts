7a42334f172bb29f1258f1db5a9a45bdda396cea
BlueSkyFish <blueskyfish@users.noreply.github.com>
2020-05-24 19:10:01 +0200

    Rename project to "endless"

---
+++ types/lib.d.ts
@@ -0,0 +1,6 @@
+export * from './file.handle';
+export * from './watcher.operators';
+export * from './watcher.observable';
+export * from './watcher.options';
+export * from './watcher.service';
+export * from './watcher.worker';
